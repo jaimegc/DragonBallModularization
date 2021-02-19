73d86abf8bb4fe1ad2fb2384416109b558130b49
Jaime Guerrero Cubero <jguerrero@carto.com>
2021-02-19 20:03:31 +0100

    Add core remote

---
+++ core/remote/src/main/java/com/jaimegc/dragonballmodularization/remote/NetworkBoundResource.kt
@@ -0,0 +1,55 @@
+package com.jaimegc.dragonballmodularization.remote
+
+import com.jaimegc.dragonballmodularization.remote.utils.ResultWrapper
+import com.jaimegc.dragonballmodularization.remote.utils.safeApiCall
+import com.jaimegc.dragonballmodularize.base.domain.DataSource
+import com.jaimegc.dragonballmodularize.base.domain.FailureData
+import com.jaimegc.dragonballmodularize.base.domain.Resource
+import com.jaimegc.dragonballmodularize.base.util.SchedulerProvider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+abstract class NetworkBoundResource<T>(private val schedulerProvider: SchedulerProvider) {
+
+    @ExperimentalCoroutinesApi
+    fun asFlow(): Flow<Resource<T>> = flow {
+        // check if should fetch data from remote or not
+        if (shouldFetch()) {
+            if (shouldFetchWithLocalData()) { // should emit cached date with loading state or not
+                emit(Resource.Success(data = localFetch(), DataSource.CACHE))
+            }
+
+            val remoteResponse = safeApiCall(dispatcher = schedulerProvider.io()) {
+                remoteFetch() // fetch the remote source provided
+            }
+
+            when (remoteResponse) {
+                is ResultWrapper.Success -> {
+                    remoteResponse.value?.let {
+                        saveFetchResult(remoteResponse.value)
+                    }
+                    emit(Resource.Success(data = remoteResponse.value, DataSource.REMOTE))
+                }
+
+                is ResultWrapper.GenericError -> {
+                    emit(
+                        Resource.Failure(
+                            failureData = FailureData(remoteResponse.code, remoteResponse.message)
+                        )
+                    )
+                }
+            }
+        } else {
+            // get from cache
+            emit(Resource.Success(data = localFetch(), DataSource.CACHE))
+        }
+    }
+
+    abstract suspend fun remoteFetch(): T
+    abstract suspend fun saveFetchResult(data: T)
+    abstract suspend fun localFetch(): T
+    open fun onFetchFailed(throwable: Throwable) = Unit
+    open fun shouldFetch() = true
+    open fun shouldFetchWithLocalData() = false
+}
---
+++ core/remote/src/main/java/com/jaimegc/dragonballmodularization/remote/utils/ErrorCodesMapper.kt
@@ -0,0 +1,17 @@
+package com.jaimegc.dragonballmodularization.remote.utils
+
+import android.content.Context
+import com.jaimegc.dragonballmodularization.remote.R
+import com.jaimegc.dragonballmodularize.base.util.NetworkCodes
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+object ErrorCodesMapper : KoinComponent {
+    private val appContext: Context by inject()
+
+    fun getMessage(errorCode: Int) = when (errorCode) {
+        NetworkCodes.CONNECTION_ERROR,
+        NetworkCodes.TIMEOUT_ERROR -> appContext.getString(R.string.connection_time_out)
+        else -> appContext.getString(R.string.generic_error)
+    }
+}
